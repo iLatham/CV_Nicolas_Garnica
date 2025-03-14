1c0e18791edd3b492d040e4027f1be7590925968
iLatham <45412907+iLatham@users.noreply.github.com>
2025-03-14 18:55:20 -0500

    Progress_Bar
    Se crea la primera barra de progreso en la sección de Habilidades la cual está asociada a HTML

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// Modo Oscuro
+
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
     document.getElementById('card').classList.toggle('dark-mode');
 }
+
+// Barra de progreso circular
+
+let circularProgress = document.querySelector(".circular-progress"),
+    progressValue = document.querySelector(".progress-value");
+let progressStartValue = 0,    
+    progressEndValue = 90,    
+    speed = 50;
+    
+let progress = setInterval(() => {
+    progressStartValue++;
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(red ${progressStartValue * 3.6}deg, #ededed 0deg)`
+    if(progressStartValue == progressEndValue){
+        clearInterval(progress);
+    }
+}, speed);
