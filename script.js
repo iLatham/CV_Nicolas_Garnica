c6df6aff577c27d70ae5180d101685a3ddb2a90e
iLatham <45412907+iLatham@users.noreply.github.com>
2025-03-14 23:09:41 -0500

    Skills_Update
    Se actualiza el módulo de habilidades agregando la barra correspondiente por cada lenguaje de programación.

--- script.js
+++ script.js
@@ -7,17 +7,47 @@ function toggleDarkMode() {
 
 // Barra de progreso circular
 
-let circularProgress = document.querySelector(".circular-progress"),
-    progressValue = document.querySelector(".progress-value");
-let progressStartValue = 0,    
-    progressEndValue = 90,    
-    speed = 50;
+let circularProgressHTML = document.querySelector(".circular-progress-html"),
+    progressValueHTML = document.querySelector(".progress-value-html");
+let progressStartValueHTML = 0,    
+    progressEndValueHTML = 90,    
+    speedHTML = 20;
     
-let progress = setInterval(() => {
-    progressStartValue++;
-    progressValue.textContent = `${progressStartValue}%`
-    circularProgress.style.background = `conic-gradient(red ${progressStartValue * 3.6}deg, #ededed 0deg)`
-    if(progressStartValue == progressEndValue){
-        clearInterval(progress);
+let progressHTML = setInterval(() => {
+    progressStartValueHTML++;
+    progressValueHTML.textContent = `${progressStartValueHTML}%`
+    circularProgressHTML.style.background = `conic-gradient(red ${progressStartValueHTML * 3.6}deg, #ededed 0deg)`
+    if(progressStartValueHTML == progressEndValueHTML){
+        clearInterval(progressHTML);
     }
-}, speed);
+}, speedHTML);
+
+let circularProgressCSS = document.querySelector(".circular-progress-css"),
+    progressValueCSS = document.querySelector(".progress-value-css");
+let progressStartValueCSS = 0,    
+    progressEndValueCSS = 70,    
+    speedCSS = 20;
+    
+let progressCSS = setInterval(() => {
+    progressStartValueCSS++;
+    progressValueCSS.textContent = `${progressStartValueCSS}%`
+    circularProgressCSS.style.background = `conic-gradient(blue ${progressStartValueCSS * 3.6}deg, #ededed 0deg)`
+    if(progressStartValueCSS == progressEndValueCSS){
+        clearInterval(progressCSS);
+    }
+}, speedCSS);
+
+let circularProgressJS = document.querySelector(".circular-progress-js"),
+    progressValueJS = document.querySelector(".progress-value-js");
+let progressStartValueJS = 0,    
+    progressEndValueJS = 50,    
+    speedJS = 20;
+    
+let progressJS = setInterval(() => {
+    progressStartValueJS++;
+    progressValueJS.textContent = `${progressStartValueJS}%`
+    circularProgressJS.style.background = `conic-gradient(purple ${progressStartValueJS * 3.6}deg, #ededed 0deg)`
+    if(progressStartValueJS == progressEndValueJS){
+        clearInterval(progressJS);
+    }
+}, speedJS);
